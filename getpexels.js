6bc950c688189479049b5d57332870afa31a970c
Shivam Sen <shivamsen910@gmail.com>
2022-04-01 19:06:14 +0200

    To Draw

--- getpexels.js
+++ getpexels.js
@@ -1,5 +1,7 @@
 import { createClient } from 'pexels';
 import * as dotenv from 'dotenv';
+import fs from "fs"
+
 
 dotenv.config();
 
@@ -8,8 +10,44 @@ const client = createClient(process.env.pexKey);
 client.photos.curated({ per_page: 1 }).then(photos => {
   var info = photos
   var photo = info["photos"]
-  console.log(photo)
   var sourCe = photo[0]
+  console.log(sourCe)
+  var h = sourCe["height"]
+  var w = sourCe["width"]
+  var avColor = sourCe["avg_color"]
   var links = sourCe["src"]
-  console.log(links["original"])
+  // json data
+  var imageData = {
+    "URL": links["original"],
+    "height": h,
+    "width": w,
+    "avg_color": avColor
+  };
+
+  console.log(imageData);
+
+  var jsonContent = JSON.stringify(imageData);
+  console.log(jsonContent);
+  fs.writeFile("./public/image.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+  });
 });
+
+
+
+// stringify JSON Object
+//var jsonContent = JSON.stringify(jsonObj);
+//console.log(jsonContent);
+
+//fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
+    //if (err) {
+		//console.log("An error occured while writing JSON Object to File.");
+        //return console.log(err);
+    //}
+
+    //console.log("JSON file has been saved.");
+//});
--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,22 @@
+var img
+
+function preload() {
+  // Get the most recent earthquake in the database
+	img = loadImage('https://images.pexels.com/photos/2903325/pexels-photo-2903325.jpeg'); // Load the image
+
+}
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background()
+
 	// put setup code here
 }
 
 function draw() {
-	background(21,132,123)
+
+
+
+
 }
